ca5909a95d4f11432aa1dce71e291a906b6b3f3c
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-05-19 05:16:03 -0600

    Initial Commit

---
+++ ThreadingProjectSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadingProjectSwift
+//
+//  Created by Italo Stuardo on 19/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
